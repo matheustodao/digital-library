57d13f83d2ac6accbe434efe4da2f78052da4624
matheustodao <todaomatheus@hotmail.com>
2022-10-21 14:48:55 -0300

    refactor: add Chakra Ui template in client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { ColorModeScript } from "@chakra-ui/react"
+import * as React from "react"
+import * as ReactDOM from "react-dom/client"
+import { App } from "./App"
+import reportWebVitals from "./reportWebVitals"
+import * as serviceWorker from "./serviceWorker"
 
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createHashHistory } from 'history';
-import configureStore from './configureStore';
-const history = createHashHistory();
 
-const initialState: any = {};
+const container = document.getElementById("root")
+if (!container) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(container)
 
-const store = configureStore(history, initialState);
-
-ReactDOM.render(
-    <App store={store} history={history} />,
-    document.getElementById("root")
-);
+root.render(
+  <React.StrictMode>
+    <ColorModeScript />
+    <App />
+  </React.StrictMode>,
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Learn more about service workers: https://cra.link/PWA
+serviceWorker.unregister()
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
+
