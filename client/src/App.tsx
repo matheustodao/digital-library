8f51065ac9761f368edf73b9b1474e624fde17a6
matheustodao <todaomatheus@hotmail.com>
2022-10-22 00:12:15 -0300

    chore: setup prettier
    style: format by prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,18 @@
-import * as React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { ChakraProvider, theme } from '@chakra-ui/react'
+import * as React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider, theme } from '@chakra-ui/react';
 
-import { ColorModeSwitcher } from './components/ColorModeSwitcher'
+import { ColorModeSwitcher } from './components/ColorModeSwitcher';
 
-import Router from './routes'
+import Router from './routes';
 
 export default function App() {
   return (
-  <BrowserRouter>
-    <ChakraProvider theme={theme}>
-      <ColorModeSwitcher />
-      <Router />
-    </ChakraProvider>
-  </BrowserRouter>
-  )
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <ColorModeSwitcher />
+        <Router />
+      </ChakraProvider>
+    </BrowserRouter>
+  );
 }
--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,5 +1,3 @@
-export default function HomePage () {
-  return (
-    <div>HomePage</div>
-  )
+export default function HomePage() {
+  return <div>HomePage</div>;
 }
