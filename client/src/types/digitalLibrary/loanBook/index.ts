f3431d180b51dad3c04dcd0689658db42d88c3ad
matheustodao <todaomatheus@hotmail.com>
2022-10-23 20:26:46 -0300

    refactor: change params loan book

--- client/src/types/digitalLibrary/loanBook/index.ts
+++ client/src/types/digitalLibrary/loanBook/index.ts
@@ -1,10 +1,14 @@
 export interface LoanBookParams {
   id: string
-  bookId: string
+  book: {
+    id: string
+    title: string
+    cover: string
+  }
   exitDate: string
   deliveryDate: string
   personName: string
-  status: string
+  status: 'no_warning' | 'first_warning' | 'second_warning' | 'third_warning' | string
   class: string | null
   teacherName: string | null
   email: string | null
