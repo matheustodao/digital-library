2f00671a4549153fa22e5093e8fabb6eb5f7bdb0
matheustodao <todaomatheus@hotmail.com>
2022-10-22 23:57:11 -0300

    refactor: change format config file eslint

---
+++ client/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'prettier',
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    '@typescript-eslint/explicit-function-return-type': 'Off',
+    '@typescript-eslint/space-before-function-paren': 'off'
+  }
+}
