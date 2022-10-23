341ca6bb5b8879e79c3a6d027bcdce4776b46bea
matheustodao <todaomatheus@hotmail.com>
2022-10-23 20:34:35 -0300

    refactor: testing list loan book and book

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,3 +1,20 @@
+import BookCard from 'src/components/Book/Card'
+import LoanBookCard from 'src/components/LoanBook/Card'
+
+import books from '@mocks/books.json'
+import loansBooks from '@mocks/loansBooks.json'
+import { Heading } from '@chakra-ui/react'
+
 export default function HomePage() {
-  return <div>HomePage</div>
+  return (
+    <>
+      <Heading as="h1">Hello World</Heading>
+      {books.results.map((book) => (
+        <BookCard book={book} key={book.id} />
+      ))}
+      {loansBooks.results.map((loan) => (
+        <LoanBookCard loan={loan} key={loan.id} />
+      ))}
+    </>
+  )
 }
