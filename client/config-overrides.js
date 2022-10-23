42b3f4ff725b7ac1af103bfa86f562261e607c01
matheustodao <todaomatheus@hotmail.com>
2022-10-23 01:31:43 -0300

    chore: add styled components
    chore: add phospor-react (icons)
    
    refactor: add config-overrides react-rewired

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,5 +1,5 @@
-const {alias, configPaths} = require('react-app-rewire-alias');
+const { alias, configPaths } = require('react-app-rewire-alias')
 
-const aliasMap = configPaths('./tsconfig.paths.json');
+const aliasMap = configPaths('./tsconfig.paths.json')
 
-module.exports = alias(aliasMap);
+module.exports = alias(aliasMap)
