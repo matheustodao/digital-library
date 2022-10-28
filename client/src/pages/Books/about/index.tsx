1c3d1f3953b5eb31c67db46d3daed289da879fc5
matheustodao <todaomatheus@hotmail.com>
2022-10-28 14:10:34 -0300

    refactor: change about book page

--- client/src/pages/Books/about/index.tsx
+++ client/src/pages/Books/about/index.tsx
@@ -1,5 +1,29 @@
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { CaretRight } from 'phosphor-react'
+import BookView from 'src/components/Book/View'
+import { BookViewContent } from 'src/components/Book/View/components/BookContent'
+import HeaderNavigationAbout from 'src/components/pages/About/HeaderAboutNavigation'
+
 export default function AboutBook() {
   return (
-    <h1>About book</h1>
+    <Stack maxW="896px" w="100%" mx="auto" width="100%" my="16" gap="32">
+      <HeaderNavigationAbout onEditionClick={() => console.log('open')} />
+      <BookView>
+        <BookViewContent.Container>
+          <Button variant="unstyled" h="min-content">
+            <BookViewContent.Title>
+              <Flex alignItems="center">
+                Descrição
+                <CaretRight />
+              </Flex>
+            </BookViewContent.Title>
+          </Button>
+
+          <BookViewContent.Text noOfLines={3}>
+            Muita gente acha que a riqueza é apenas uma questão de nascer rico ou ter muita sorte. Mas a verdade é que é possível fazer seu dinheiro render e gerar riqueza. Você só precisa de informação
+          </BookViewContent.Text>
+        </BookViewContent.Container>
+      </BookView>
+    </Stack>
   )
 }
