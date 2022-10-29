80d624c076fbd16bbf2c446fb77b2868fdb0339e
matheustodao <todaomatheus@hotmail.com>
2022-10-28 22:01:23 -0300

    refactor: add route about loaned book page

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -4,6 +4,7 @@ import HomePage from '@pages/Home'
 import BooksPage from 'src/pages/Books/home'
 import LoansBooksPage from 'src/pages/LoansBooks/home'
 import AboutBook from 'src/pages/Books/about'
+import AboutLoanedBook from 'src/pages/LoansBooks/about'
 
 export default function Router() {
   return (
@@ -13,6 +14,7 @@ export default function Router() {
       <Route path="/books" element={<BooksPage />} />
       <Route path="/books/:id" element={<AboutBook />} />
       <Route path="/loans" element={<LoansBooksPage/>} />
+      <Route path="/loans/:id" element={<AboutLoanedBook />} />
     </Routes>
   )
 }
