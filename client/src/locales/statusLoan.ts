38f27605efe46f916bd2b6b3a4d78937d3d0c910
matheustodao <todaomatheus@hotmail.com>
2022-10-28 22:00:37 -0300

    refactor: add loanStatus color and translation

---
+++ client/src/locales/statusLoan.ts
@@ -0,0 +1,29 @@
+export const loanStatus = {
+  no_warning: {
+    color: '#99CF63',
+    translation: {
+      'pt-bt': 'Sem Aviso'
+    }
+  },
+
+  first_warning: {
+    color: '#FBED6A',
+    translation: {
+      'pt-br': '1º aviso'
+    }
+  },
+
+  second_warning: {
+    color: '#FBB06A',
+    translation: {
+      'pt-br': '2º aviso'
+    }
+  },
+
+  third_warning: {
+    color: '#FC8A8A',
+    translation: {
+      'pt-br': '3º aviso'
+    }
+  }
+}
