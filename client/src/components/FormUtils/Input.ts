6974d66087e06ab5cb2d36528907ad5de15a1223
matheustodao <todaomatheus@hotmail.com>
2022-11-14 20:16:39 -0300

    refactor: add custom input

---
+++ client/src/components/FormUtils/Input.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+import { Input as InputChakra } from '@chakra-ui/react'
+
+export const Input = styled(InputChakra).attrs(() => ({
+  focusBorderColor: 'orange.500'
+}))``
