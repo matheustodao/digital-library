99e5224e5cb7205306f9544661e84407d97a4ad4
matheustodao <todaomatheus@hotmail.com>
2022-11-15 05:14:37 -0300

    refactor: add dynamic logo component

---
+++ client/src/components/Logo/mock.ts
@@ -0,0 +1,76 @@
+// Logos with Details
+import logoWhite from '@assets/images/logo-white.svg'
+import logoDark from '@assets/images/logo-dark.svg'
+
+// Dark Logo No Details
+import logoDarkNoDetails from '@assets/images/logo-dark-no-details.svg'
+import logoWhiteNoDetails from '@assets/images/logo-white-no-details.svg'
+
+export const logosMocks = {
+  white: {
+    lg: {
+      src: logoWhite,
+      width: 711,
+      height: 215
+    },
+
+    md: {
+      src: logoWhite,
+      width: 243,
+      height: 83
+    },
+    sm: {
+      src: logoWhite,
+      width: 178,
+      height: 64
+    }
+  },
+
+  dark: {
+    lg: {
+      src: logoDark,
+      width: 711,
+      height: 215
+    },
+    md: {
+      src: logoDark,
+      width: 243,
+      height: 83
+    },
+    sm: {
+      src: logoDark,
+      width: 178,
+      height: 64
+    }
+  },
+
+  'no-details': {
+    white: {
+      lg: {
+        src: logoWhiteNoDetails,
+        width: 295,
+        height: 53
+      },
+
+      sm: {
+        src: logoWhiteNoDetails,
+        width: 154,
+        height: 28
+      }
+    },
+
+    dark: {
+      lg: {
+        src: logoDarkNoDetails,
+        width: 295,
+        height: 53
+      },
+
+      sm: {
+        src: logoDarkNoDetails,
+        width: 154,
+        height: 28
+      }
+    }
+  }
+}
