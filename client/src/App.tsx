355c63a11e6dc0e199c773633a6bd7c4f1559479
matheustodao <todaomatheus@hotmail.com>
2022-11-15 15:03:13 -0300

    chore: setup react-toastify

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
 import { ThemeProvider } from 'styled-components'
 import { HashRouter } from 'react-router-dom'
 import { ChakraProvider, theme } from '@chakra-ui/react'
+import { ToastContainer } from 'react-toastify'
 
 import Router from './routes'
+
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function App() {
   return (
     <HashRouter>
+      <ToastContainer
+        theme="colored"
+        autoClose={2000}
+      />
       <ChakraProvider theme={theme}>
         <ThemeProvider theme={theme}>
           <Router />
