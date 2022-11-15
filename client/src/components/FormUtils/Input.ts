ca879463393bb7c9bad2d3be1297c5e4548ec03a
matheustodao <todaomatheus@hotmail.com>
2022-11-15 05:13:36 -0300

    refactor: add types into custom input

--- client/src/components/FormUtils/Input.ts
+++ client/src/components/FormUtils/Input.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
-import { Input as InputChakra } from '@chakra-ui/react'
+import { Input as InputChakra, InputProps } from '@chakra-ui/react'
 
-export const Input = styled(InputChakra).attrs(() => ({
+export const Input = styled(InputChakra).attrs((): InputProps => ({
   focusBorderColor: 'orange.500'
-}))``
+}))<InputProps>``
