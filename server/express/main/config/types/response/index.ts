799475d0552fa40f776704564d5bcc0d587916fa
than-dev <nathan.cotrim@gmail.com>
2022-11-15 01:25:28 -0300

    fix: response types

--- server/express/main/config/types/response/index.ts
+++ server/express/main/config/types/response/index.ts
@@ -1,11 +1,12 @@
-import { LoanBookParams } from './../loanBook/index'
-import { BookParams } from '../book'
+import { BookLoanParams } from './../loanBook/index';
+import { BookParams } from '../book';
 
 export interface ResponsePatternApi<DataType = any> {
-  pages: number
-  perPage: number
-  results: DataType
+	pages: number;
+	perPage: number;
+	results: DataType;
 }
 
-export interface BookResponseParams extends ResponsePatternApi<BookParams> { }
-export interface LoanBookResponseParams extends ResponsePatternApi<LoanBookParams> { }
+export interface BookResponseParams extends ResponsePatternApi<BookParams> {}
+export interface LoanBookResponseParams
+	extends ResponsePatternApi<BookLoanParams> {}
