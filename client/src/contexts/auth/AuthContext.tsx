257d2c5bdca6629b851d12172f68b6d3a50a31f1
matheustodao <todaomatheus@hotmail.com>
2022-11-15 22:07:12 -0300

    refactor: add auth context
    refactor: add useAuth

---
+++ client/src/contexts/auth/AuthContext.tsx
@@ -0,0 +1,63 @@
+import { invalidCredentials } from '@infra/errors/digitalLibrary/status/400/invalidCredentials'
+import { configServices } from '@services/digitalLibrary/config'
+import { AuthConfigParams, AuthLoginParams } from '@type/digitalLibrary/auth'
+import { createContext, useMemo, useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+
+interface AuthContextProps {
+  configs: AuthConfigParams
+  isAuthenticated: boolean
+  handleSignIn: (params: AuthLoginParams) => Promise<any>
+  handleSetupConfig: (params?: AuthConfigParams) => void
+  handleSignOut: () => void
+}
+
+export const AuthContext = createContext({} as AuthContextProps)
+
+export default function AuthProvider({ children }: any) {
+  const navigate = useNavigate()
+  const configsRef = useRef<AuthConfigParams>({} as AuthConfigParams)
+  const isAuthenticatedRef = useRef(!configsRef.current)
+
+  function handleSetupConfig(data?: AuthConfigParams) {
+    if (!data) return
+
+    configsRef.current = { ...configsRef.current, ...data }
+  }
+
+  async function handleSignIn(params: AuthLoginParams) {
+    try {
+      const data: AuthConfigParams = await configServices.login(params)
+      isAuthenticatedRef.current = true
+
+      handleSetupConfig(data)
+      navigate('/home', { replace: true })
+      toast.success('Login realizado com sucesso')
+    } catch (err) {
+      if (err instanceof Error) {
+        invalidCredentials(err)
+      }
+    }
+  }
+
+  function handleSignOut() {
+    configsRef.current = {} as never
+    navigate('/login', { replace: true })
+    isAuthenticatedRef.current = false
+  }
+
+  const values = useMemo(() => ({
+    configs: configsRef.current,
+    isAuthenticated: isAuthenticatedRef.current,
+    handleSignIn: async (params: AuthLoginParams) => await handleSignIn(params),
+    handleSetupConfig: (params?: AuthConfigParams) => handleSetupConfig(params),
+    handleSignOut: () => handleSignOut()
+  }), [configsRef.current, isAuthenticatedRef.current])
+
+  return (
+    <AuthContext.Provider value={values}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
