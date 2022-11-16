eb36016a411fed6895f982d525adaf87cad3c0de
matheustodao <todaomatheus@hotmail.com>
2022-11-15 23:55:16 -0300

    refactor: change placeholder font size

--- client/src/components/FormUtils/Input.ts
+++ client/src/components/FormUtils/Input.ts
@@ -2,5 +2,8 @@ import styled from 'styled-components'
 import { Input as InputChakra, InputProps } from '@chakra-ui/react'
 
 export const Input = styled(InputChakra).attrs((): InputProps => ({
-  focusBorderColor: 'orange.500'
+  focusBorderColor: 'orange.500',
+  _placeholder: {
+    fontSize: '14px'
+  }
 }))<InputProps>``
