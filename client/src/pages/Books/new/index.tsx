2b55bd3542d4705dcfa560b3197ead5002cd6c80
matheustodao <todaomatheus@hotmail.com>
2022-11-16 18:13:33 -0300

    refactor: add auto complete field

--- client/src/pages/Books/new/index.tsx
+++ client/src/pages/Books/new/index.tsx
@@ -4,11 +4,45 @@ import { Box, Button, Flex, Icon, useColorModeValue } from '@chakra-ui/react'
 import Title from '@components/pages/Title'
 import { useNavigate } from 'react-router-dom'
 import { CaretLeft } from 'phosphor-react'
+import { useCallback, useEffect, useRef } from 'react'
+import { googleBookServices } from '@services/digitalLibrary/googleBooks'
 
 export default function NewBookPage() {
   const navigation = useNavigate()
   const methods = useForm()
   const currentBgColor = useColorModeValue('white', 'gray.800')
+  const fetchedBookInformation = useRef({})
+  const isbn = methods.watch('isbn')
+
+  const findBookInformation = useCallback(async () => {
+    if (!isbn?.trim()) return
+    const data = await googleBookServices.index({ q: isbn.trim() }) as { totalItems: number, items: any[] }
+    if (data.totalItems === 0) {
+      fetchedBookInformation.current = { }
+      return
+    }
+
+    setTimeout(() => {
+      const book = data.items.find((currentItem) => currentItem.volumeInfo?.publisher).volumeInfo
+
+      const parsedBook = {
+        title: !book?.subtitle ? book.title : `${book.title} - ${book.subtitle}`,
+        authors: book?.authors.join(', '),
+        description: book?.description,
+        cover: book?.imageLinks.thumbnail,
+        categories: book?.categories.join(', '),
+        publishingCompany: book.publisher
+
+      }
+
+      fetchedBookInformation.current = { ...fetchedBookInformation.current, ...parsedBook }
+      methods.reset(parsedBook)
+    }, 500)
+  }, [isbn])
+
+  useEffect(() => {
+    findBookInformation()
+  }, [findBookInformation])
 
   return (
     <FormProvider {...methods}>
