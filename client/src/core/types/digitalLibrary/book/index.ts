1ea2dc974c921b0b2250435f226cfbbf46ba8de5
matheustodao <todaomatheus@hotmail.com>
2022-11-17 00:31:52 -0300

    refactor: add NewBookParams types

--- client/src/core/types/digitalLibrary/book/index.ts
+++ client/src/core/types/digitalLibrary/book/index.ts
@@ -4,9 +4,17 @@ export interface BookParams {
   authors: string[]
   categories: string[]
   tumble: string
-  isbn: string
   publishingCompany: string
+  isbn: string
   description: string
   cover: string
   quantity: number
+}
+
+export interface NewBookParams extends Omit<BookParams, 'authors' | 'categories' | 'id' | 'isbn' | 'cover' | 'description'> {
+  authors: string
+  categories: string
+  isbn: string | null
+  description: string | null
+  cover: string | null
 }
