9225853ba24685613dbd8f3c6c29bc8f0cb5e6f8
matheustodao <todaomatheus@hotmail.com>
2022-11-17 00:32:31 -0300

    refactor: add create book use case

---
+++ client/src/core/domain/services/digitalLibrary/books/usecases/create.ts
@@ -0,0 +1,32 @@
+import MissingParam from '@infra/errors/MissingParam'
+import { NewBookParams } from '@type/digitalLibrary/book'
+
+export class CreateBookUseCase {
+  handleParams(params: NewBookParams) {
+    const validParams = {}
+
+    Object.entries(params).forEach(([key, value]) => {
+      if (key === 'isbn' || key === 'description' || key === 'cover') {
+        Object.assign(validParams, { [key]: value ?? '' })
+      }
+
+      if (!value) {
+        throw new MissingParam(key)
+      }
+
+      if (key === 'quantity') {
+        return Object.assign(validParams, { [key]: value === 0 ? 1 : Number(value) })
+      }
+
+      if (key === 'categories' || key === 'authors') {
+        return Object.assign(validParams, { [key]: value.split(',') })
+      }
+
+      Object.assign(validParams, { [key]: value })
+    })
+
+    return validParams
+  }
+}
+
+export const createBookUseCase = new CreateBookUseCase()
