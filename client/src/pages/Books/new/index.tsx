a9482ead3bbf4e64d42b1d060b55d737c17840d0
matheustodao <todaomatheus@hotmail.com>
2022-11-22 05:11:45 -0300

    refactor: add header form

--- client/src/pages/Books/new/index.tsx
+++ client/src/pages/Books/new/index.tsx
@@ -1,9 +1,7 @@
 import { FormProvider, useForm } from 'react-hook-form'
 import { FormBook } from '@components/Book/Form'
-import { Box, Button, Flex, Icon, useColorModeValue } from '@chakra-ui/react'
-import Title from '@components/pages/Title'
+import { Box, Button } from '@chakra-ui/react'
 import { useNavigate } from 'react-router-dom'
-import { CaretLeft } from 'phosphor-react'
 
 import { googleBookServices } from '@services/googleBooks'
 import { toast } from 'react-toastify'
@@ -11,9 +9,9 @@ import { NewBookParams } from '@type/book'
 import { booksServices } from '@services/books'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { createBookSchemaValidation } from '@validations/yup/digitalLibrary/book/create'
+import HeaderForm from '@components/Book/Form/components/HeaderForm'
 
 export default function NewBookPage() {
-  const currentBgColor = useColorModeValue('white', 'gray.800')
   const navigation = useNavigate()
   const methods = useForm<NewBookParams>({
     resolver: yupResolver(createBookSchemaValidation),
@@ -78,45 +76,22 @@ export default function NewBookPage() {
   return (
     <FormProvider {...methods}>
       <Box as="form" position="relative" onSubmit={methods.handleSubmit(handleOnSubmit)}>
-        <Box as="header" position="sticky" top="10px" left="0" maxW="1300px" mx="auto" p="5" borderRadius="sm" bgColor={currentBgColor} zIndex="sticky">
-          <Flex alignItems="center" justifyContent="space-between">
-            <Button
-              onClick={() => navigation(-1)}
-              display="flex"
-              type="button"
-              alignItems="center" justifyContent="flex-start"
-              variant="unstyled"
-              gap={2}
-            >
-              <Icon
-                as={CaretLeft}
-                w="40px"
-                h="40px"
-                color="currentcolor"
-              />
-              <Title lineHeight="none" margin={0}>Novo Livro</Title>
-            </Button>
-
-            <Button
-              type="submit"
-              size="lg"
-              bgColor="orange.500"
-              color="white"
-              transition="all ease .25s"
-              _hover={{ bgColor: 'orange.600' }}
-              _active={{ bgColor: 'orange.500' }}
-            >
-              Cadastrar
-            </Button>
-          </Flex>
-        </Box>
+        <HeaderForm label="Cadastrar" />
 
         {/* Form Book Component */}
         <FormBook
           fields={{
             _isbn: {
               description: (
-              <Button variant="link" size="sm" fontSize="0.875rem" fontWeight="thin" color="GrayText" mb="-10px" onClick={handleFindBookInformation}>
+              <Button
+                variant="link"
+                size="sm"
+                textDecor="underline"
+                fontSize="0.875rem"
+                fontWeight="thin"
+                color="blue.600"
+                mb="-10px" onClick={handleFindBookInformation}
+              >
                 Buscar informações do livro pelo ISBN
               </Button>
               )
