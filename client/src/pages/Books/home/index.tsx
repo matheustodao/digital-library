0d28ab70ed9b587f6b8e8ed0e49b1068884f4748
matheustodao <todaomatheus@hotmail.com>
2022-11-22 22:29:48 -0300

    feat: ascending/descending books

--- client/src/pages/Books/home/index.tsx
+++ client/src/pages/Books/home/index.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import BookCard from '@components/Book/Card'
 
-import { Box, Flex, SimpleGrid } from '@chakra-ui/react'
+import { Box, Button, Flex, SimpleGrid } from '@chakra-ui/react'
 import HeaderPage from '@components/pages/HeaderPage'
-import { Plus } from 'phosphor-react'
+import { Plus, SortAscending, SortDescending } from 'phosphor-react'
 import Title from '@components/pages/Title'
 import { useNavigate } from 'react-router-dom'
 import { useCallback, useEffect, useState } from 'react'
@@ -11,15 +11,25 @@ import { booksServices } from '@services/books'
 import { BookParams } from '@type/book'
 import BooksList from './components/BooksList'
 import NotBookFound from '@components/Book/errors/NotBookFound'
+import SortButton from '@components/Filters/ButtonsFilter/SortButton'
 
 export default function BooksPage() {
   const navigate = useNavigate()
   const [books, setBooks] = useState<BookParams[]>([] as BookParams[])
+  const [sortBook, setSortBook] = useState<'asc' | 'desc'>('asc')
 
   const loadBooks = useCallback(async () => {
-    const data = await booksServices.index()
+    const data = await booksServices.index({
+      filters: {
+        orderBy: sortBook
+      }
+    })
     setBooks(data)
-  }, [])
+  }, [sortBook])
+
+  function handleToggleSortBook() {
+    setSortBook((oldState) => (oldState === 'asc' ? 'desc' : 'asc'))
+  }
 
   useEffect(() => {
     loadBooks()
@@ -44,6 +54,10 @@ export default function BooksPage() {
             placeholder: 'Pesquise por autor, titulo do livro, sinopse...'
           }}
         />
+
+        <Box mt="18px">
+          <SortButton sort={sortBook} onSort={handleToggleSortBook} />
+        </Box>
       </Box>
 
       <BooksList books={books} />
