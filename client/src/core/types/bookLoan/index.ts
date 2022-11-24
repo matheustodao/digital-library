7b2511665460cae1c2592c5fb6ef29f2607b0a5e
matheustodao <todaomatheus@hotmail.com>
2022-11-24 20:10:13 -0300

    refactor: update type book loan

--- client/src/core/types/bookLoan/index.ts
+++ client/src/core/types/bookLoan/index.ts
@@ -7,16 +7,14 @@ type LoanBookStatus = 'no_warning' | 'first_warning' | 'second_warning' | 'third
 export interface BookLoanParams {
   id: string
   book: LoanedBookParams
-
   exitDate: string
   deliveryDate: string
   personName: string
   status: LoanBookStatus
-
-  class: string | null
-  teacherName: string | null
-  email: string | null
-  phone: string | null
+  class?: string | null
+  teacherName?: string | null
+  email?: string | null
+  phone?: string | null
 }
 
 export interface CreateBookLoanParams extends Omit<BookLoanParams, 'id' | 'book' | 'status'> {
