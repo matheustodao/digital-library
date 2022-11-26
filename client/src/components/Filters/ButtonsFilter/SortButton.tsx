f4d5ced4fa40293770ae332eb25b7d33829e7f3f
matheustodao <todaomatheus@hotmail.com>
2022-11-26 16:14:20 -0300

    refactor: add a custom icon button

--- client/src/components/Filters/ButtonsFilter/SortButton.tsx
+++ client/src/components/Filters/ButtonsFilter/SortButton.tsx
@@ -1,5 +1,5 @@
-import { Button, ButtonProps } from '@chakra-ui/react'
-import React from 'react'
+import { Button, ButtonProps, Text } from '@chakra-ui/react'
+import React, { ReactElement } from 'react'
 import SortIcon from './components/SortIcon'
 
 interface SortButtonProps {
@@ -7,24 +7,26 @@ interface SortButtonProps {
   onSort: () => void
   ascLabel?: string | null
   descLabel?: string | null
+  iconButton?: ReactElement | null
   _buttonProps?: ButtonProps
 }
 
-export default function SortButton({ onSort, sort, ascLabel, descLabel, _buttonProps }: SortButtonProps) {
+export default function SortButton({ onSort, sort, ascLabel, descLabel, iconButton, _buttonProps }: SortButtonProps) {
   return (
     <Button
       bg="orange.100"
       color="orange.900"
-      leftIcon={<SortIcon sort={sort} />}
+      leftIcon={iconButton ?? <SortIcon sort={sort} />}
       transition="all .35s ease"
       _hover={{ bg: 'orange.200' }}
       _active={{ bg: 'orange.200' }}
       onClick={onSort}
+      flex="175px 1 100px"
       {..._buttonProps}
     >
-      <span style={{ minWidth: '96px' }}>
+      <Text as="span" noOfLines={1}>
         {sort === 'asc' ? ascLabel : descLabel}
-      </span>
+      </Text>
     </Button>
   )
 }
