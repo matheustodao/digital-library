e1acf0f779fc1085a2a2a10edd7d1fbedc659fdf
matheustodao <todaomatheus@hotmail.com>
2022-11-27 19:48:29 -0300

    refactor: add chart loans by month

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,16 +1,30 @@
-import BookCard from '@components/Book/Card'
-
-import books from '@mocks/books.json'
 import Title from '@components/pages/Title'
+import { useEffect, useState } from 'react'
+import { reportsServices } from '@services/reports'
+import { GraphLineType } from '@type/lib/nivo'
+import ResponsiveLineCustom from '@components/Charts/Responsive/Line'
+import { Box } from '@chakra-ui/react'
 
 export default function HomePage() {
+  const [byMonthLoans, setByMonthLoans] = useState([] as GraphLineType[])
+
+  async function loadByMonthLoans() {
+    const data = await reportsServices.byMonthLoans()
+    setByMonthLoans(data)
+  }
+
+  useEffect(() => {
+    loadByMonthLoans()
+  }, [])
+
   return (
     <>
-      <Title>Home</Title>
+      <Title fontWeight="black" color="orange.500" opacity={0.9}>Dashboard</Title>
 
-      {books.results.map((book) => (
-        <BookCard book={book} key={book.id} />
-      ))}
+      <Box>
+        <Title as="h2" fontWeight="bold">Empréstimos mensal de {new Date().getFullYear()}</Title>
+        <ResponsiveLineCustom data={byMonthLoans} />
+      </Box>
     </>
   )
 }
