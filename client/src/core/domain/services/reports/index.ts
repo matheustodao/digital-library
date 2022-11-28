fedaf212288f6a1b544e9028a177e2641a19c200
matheustodao <todaomatheus@hotmail.com>
2022-11-28 01:44:28 -0300

    refactor: add genera data report
    refactor: add use case general data

--- client/src/core/domain/services/reports/index.ts
+++ client/src/core/domain/services/reports/index.ts
@@ -22,6 +22,20 @@ class ReportsServices extends HttpClientDigitalLibrary {
 
     return dataParsed
   }
+
+  async getGeneralData() {
+    const data = await this.httpClient.get({ path: '/dashboard' })
+    const studentLoans = this.usecase.generalData.formatToPieGraphNivo('Alunos', data.studentLoans)
+    const employeeLoans = this.usecase.generalData.formatToPieGraphNivo('Funcionários', data.employeeLoans)
+    const totalBooks = this.usecase.generalData.formatToPieGraphNivo('Total de Livros', data.booksQuantity)
+    const totalLoans = this.usecase.generalData.formatToPieGraphNivo('Total de Empréstimos', data.bookLoansQuantity)
+    const dataParsed = {
+      studentsComparedToEmployee: [studentLoans, employeeLoans],
+      booksComparedToLoans: [totalBooks, totalLoans]
+    }
+
+    return dataParsed
+  }
 }
 
 export const reportsServices = new ReportsServices()
