305c85a55f281802890315a5f0c37e7e8e533f32
matheustodao <todaomatheus@hotmail.com>
2022-12-04 13:15:08 -0300

    refactor: transform component into forward ref

--- client/src/pages/Books/home/components/BooksList.tsx
+++ client/src/pages/Books/home/components/BooksList.tsx
@@ -1,18 +1,21 @@
 import { Flex, FlexProps } from '@chakra-ui/react'
 import BookCard from '@components/Book/Card'
 import { BookParams } from '@type/book'
+import { forwardRef } from 'react'
 
 interface BooksListProps {
   books: BookParams[]
   _flexProps?: FlexProps
 }
 
-export default function BooksList({ books, _flexProps }: BooksListProps) {
+const BooksList = forwardRef(({ books, _flexProps }: BooksListProps, currentRef) => {
   return (
-    <Flex flexWrap="wrap" gap="32px 24px" {..._flexProps}>
+    <Flex flexWrap="wrap" gap="32px 24px" {..._flexProps} ref={currentRef as never}>
       {books.map((book) => (
         <BookCard book={book} key={book.id} />
       ))}
     </Flex>
   )
-}
+})
+
+export default BooksList
