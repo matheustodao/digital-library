5d69c985f19314941df6949d998e71e051ca4a87
matheustodao <todaomatheus@hotmail.com>
2022-12-06 13:59:10 -0300

    refactor: add current page prop
    refactor: add start index
    
    refactor: change end index

--- client/src/components/Pagination/index.tsx
+++ client/src/components/Pagination/index.tsx
@@ -4,15 +4,21 @@ import { ButtonPage, ButtonProps } from './components/ButtonPage'
 
 interface PaginationProps {
   totalPages: number
+  currentPage: number
   _buttonPageProps?: ButtonProps
 }
 
-const Pagination = forwardRef(({ totalPages, _buttonPageProps }: PaginationProps, ref) => {
+const Pagination = forwardRef(({ totalPages, currentPage, _buttonPageProps }: PaginationProps, ref) => {
   const arrPages: number[] = [...Array.from({ length: totalPages }).keys()]
   const [smallerThan630] = useMediaQuery('(max-width: 630px)')
   const [smallerThan472] = useMediaQuery('(max-width: 472px)')
+  const startIndex = useMemo(() => {
+    if (smallerThan472) return currentPage
+    return 0
+  }, [smallerThan472])
+
   const endIndex = useMemo(() => {
-    if (smallerThan472) return 1
+    if (smallerThan472) return currentPage + 1
     if (smallerThan630) return 3
     return 5
   }, [smallerThan472, smallerThan630])
@@ -22,24 +28,35 @@ const Pagination = forwardRef(({ totalPages, _buttonPageProps }: PaginationProps
       <ButtonPage content="<" />
 
       <Flex gap="12px">
-
-        {arrPages.slice(0, endIndex).map((value, index) => (
-          <ButtonPage key={`page-${index}`} content={value + 1} {..._buttonPageProps} />
+        {arrPages.slice(startIndex, endIndex).map((value, index) => (
+          <ButtonPage
+            key={`page-${index}`}
+            content={value + 1}
+            isActive={value === currentPage}
+            {..._buttonPageProps}
+          />
         ))}
 
-        <ButtonPage
-          content="..."
-          _buttonProps={{
-            disabled: true,
-            _hover: { bg: 'none' },
-            cursor: 'default !important'
-          }}
-        />
+        {smallerThan472 && (
+          <>
+            <ButtonPage
+              content="..."
+              _buttonProps={{
+                disabled: true,
+                _hover: { bg: 'none' },
+                cursor: 'default !important'
+              }}
+            />
 
-        {arrPages.slice(totalPages - (endIndex === 5 ? 3 : endIndex)).map((value) => (
+            {arrPages.slice(totalPages - 1).map((value) => (
+              <ButtonPage key={`page-${value}`} content={value + 1} {..._buttonPageProps} />
+            ))}
+          </>
+        )}
+
+        {!smallerThan472 && arrPages.slice(totalPages - 2).map((value) => (
           <ButtonPage key={`page-${value}`} content={value + 1} {..._buttonPageProps} />
         ))}
-
       </Flex>
 
       <ButtonPage content=">" />
