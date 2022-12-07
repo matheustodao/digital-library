fb4e5870bfca850a62f2044d22b48540a41123c1
matheustodao <todaomatheus@hotmail.com>
2022-12-07 10:46:33 -0300

    refactor: add responseType as blob

--- client/src/core/domain/services/Export/index.ts
+++ client/src/core/domain/services/Export/index.ts
@@ -2,7 +2,7 @@ import { HttpClientDigitalLibrary } from '@infra/Apis/digitalLibraryApi'
 
 class ExportDataServices extends HttpClientDigitalLibrary {
   async export(data: { format: 'pdf' | 'xlsx', content: 'books' | 'loans' }) {
-    return this.httpClient.post({ path: '/export', data })
+    return this.httpClient.post({ path: '/export', data, options: { responseType: 'blob' } })
   }
 }
 
