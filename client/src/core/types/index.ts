8fe4b385f4450279966d87d5f4cc6e7bac435484
matheustodao <todaomatheus@hotmail.com>
2022-12-07 04:09:48 -0300

    refactor: add export and import params

--- client/src/core/types/index.ts
+++ client/src/core/types/index.ts
@@ -2,3 +2,14 @@ export interface FilterOptionsType {
   text?: string
   orderBy?: 'asc' | 'desc'
 }
+
+export interface ExportParams {
+  content: 'books' | 'loans'
+  format: 'xlsx' | 'pdf'
+}
+
+export interface ImportParams {
+  type: 'books' | 'loans'
+  keepData: 'replace' | 'maintain'
+  file: File[]
+}
