7778299230367a35dedb197c9c9d3ee0264c2ba1
matheustodao <todaomatheus@hotmail.com>
2022-12-07 11:58:58 -0300

    refactor: custom key file name

--- client/src/core/domain/usecases/import/usecase/common.ts
+++ client/src/core/domain/usecases/import/usecase/common.ts
@@ -1,10 +1,10 @@
 import { ImportParams } from '@type/index'
 
 export class CommonMethodsUseCase {
-  handleData(data: Omit<ImportParams, 'type'>) {
+  handleData(data: Omit<ImportParams, 'type'>, paramNameFile: string = 'file') {
     const formData = new FormData()
     formData.append('keepData', data.keepData === 'maintain' ? '1' : '0')
-    formData.append('file', data.file[0])
+    formData.append(paramNameFile, data.file[0])
 
     return formData
   }
